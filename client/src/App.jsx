512bce5b35572de2a47ab2016072e0479458eb08
Jin kazama <klaus-0-0>
2025-03-28 11:55:00 +0530

    update few files for deployment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import ChatBox from "./components/ChatBox";
+import TestingUi from "./components/TestingUi";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/chatbox" element={<ChatBox />} />
+        <Route path="/testingUi" element={<TestingUi />} />
       </Routes>
     </Router>
   );
---
+++ client/src/components/TestingUi.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import chatImg from '../assets/hello.svg'
+import { useNavigate } from "react-router-dom";
+
+const TestingUi = () => {
+
+    const [animation, setanimation] = useState(false)
+    const navigate = useNavigate()
+
+    const handleLogin = () => {
+
+        setanimation(true);
+        setTimeout(() => {
+            navigate('/Chatbox')
+        }, 1200);
+    };
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white p-4">
+            <div className="flex flex-col items-center gap-4 p-6 bg-transparent rounded-lg shadow-lg w-80">
+                <img src={chatImg} className="w-48 h-48 object-contain" alt="Chat" />
+                <button
+                    onClick={handleLogin}
+                    className={`relative mt-4 flex items-center justify-center w-16 h-16 text-lg font-semibold text-white 
+                      bg-gradient-to-r from-green-200 to-green-600 rounded-full shadow-xl 
+                      transition-all duration-700 ease-in-out transform border-1
+                      ${animation ? "border-green-500" : ""}
+                      ${animation ? "" : "hover:scale-110 animate-glow-green"}`}
+                >
+                    {animation ? "✔" : ">"}
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default TestingUi;
